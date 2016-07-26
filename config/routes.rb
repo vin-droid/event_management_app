e4ff5b765bee21c52231823514cf523be1b345e8
vin-droid <vineet.12cs069@abes.ac.in>
2016-07-26 10:52:26 +0530

    routes for friendship added

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only:[:show] do 
+    get 'friends' => 'friendships#friends'
   end
   resources :events do 
     end
-	  get 'send_request'      => 'friendships#send_requsest',   as: :send_friendship_request
+	  get 'friends/requests/sent'      => 'friendships#sent_requsests',   as: :sent_friendship_requests
+    get 'friends/requests/recieved' => 'friendships#recieved_requests', as: :recieved_friendship_requests
+
+    get 'send_request' => 'friendships#send_requsest', as: :send_friendship_request
     delete 'cancel_request' => 'friendships#cancel_requsest', as: :cancel_friendship_request
     get  'accept_request'   => 'friendships#accept_requsest', as: :accept_friendship_request
+   
+
 end
